be98b8bd89114489c4e85c943d423e1c209a762d
Muhammad Faisal <faisalazam_1616@hotmail.com>
2023-07-26 15:46:44 +0500

    Organised code.

---
+++ lib/utils/RoutesManager.dart
@@ -0,0 +1,10 @@
+import 'package:animated_splash/widgets/animated_splash_page.dart';
+import 'package:flutter/material.dart';
+
+class RoutesManager {
+  static String entryPoint = AnimatedSplashPage.routeName;
+  static Map<String, WidgetBuilder> routes = <String, WidgetBuilder>{
+    AnimatedSplashPage.routeName: (context) =>
+        const AnimatedSplashPage(title: 'Flutter - Animated Splash'),
+  };
+}
